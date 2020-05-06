89c590c3b4da64a3a8c80df102c1c1ddec43b09d
pjay79 <praveen.jayarajan@gmail.com>
2020-05-06 10:33:34 +1000

    swagger setup

--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const app = express();
-
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const contactController = require('../controllers/contactController');
 
+const options = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Node.js Express REST API Docs",
+      version: "1.0.0",
+      description:
+        "Contacts API documentation for endpoints",
+      license: {
+        name: "MIT",
+        url: "https://choosealicense.com/licenses/mit/"
+      },
+      contact: {
+        name: "Praveen Jayarajan",
+        url: "https://praveen-jayarajan.squarespace.com",
+        email: "praveen.jayarajan@gmail.com"
+      }
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api/v1"
+      }
+    ]
+  },
+  apis: []
+};
+
+const specs = swaggerJsdoc(options);
+
+router.use('/docs', swaggerUi.serve);
+router.get('/docs', swaggerUi.setup(specs, { explorer: true }));
 router.get('/', (req, res) => res.status(200).send('Node Express API'));
 router.get('/contact', contactController.allContacts);
 router.post('/contact', contactController.createContact);
