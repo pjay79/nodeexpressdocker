eff67a8b1f5efd032849ca69acde2053d31af998
pjay79 <praveen.jayarajan@gmail.com>
2020-05-06 14:25:43 +1000

    refactor swagger setup

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDoc = require('./src/apiDocs/swaggerDoc.json');
 const contactRouter = require('./src/routes/contactRoutes');
 
+const options = {
+  explorer: true
+};
+ 
 const PORT = 8080;
 
 app.use(express.static(__dirname));
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', contactRouter)
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc, options));
 
 app.listen(PORT, () => {
   console.log('Server running successfully');
--- src/routes/contactRoutes.js
+++ src/routes/contactRoutes.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const app = express();
-const swaggerJsdoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('../apiDocs/swagger');
 const contactController = require('../controllers/contactController');
 
-const specs = swaggerJsdoc(swaggerSpec);
-
-router.use('/docs', swaggerUi.serve);
-router.get('/docs', swaggerUi.setup(specs, { explorer: true }));
 router.get('/', (req, res) => res.status(200).send('Node Express API'));
 router.get('/contact', contactController.allContacts);
 router.post('/contact', contactController.createContact);
